c90c04758de3a366e1e02196b35f2cde495a1f04
Keithcutt <mayerhofnew@gmail.com>
2024-02-05 22:47:55 +0300

    Выполненное домашнее задание 8 спринта: 1. Свёрстаны главный экран и экран настроек 2. Реализованы нажатия на кнопки главного экрана и кнопку "назад" экрана настроек.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<MaterialButton>(R.id.search_btn)
+        val mediaButton = findViewById<MaterialButton>(R.id.media_btn)
+        val settingsButton = findViewById<MaterialButton>(R.id.settings_btn)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
