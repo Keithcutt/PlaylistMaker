26c60afc69466fe5828e7e894c3de65a5d1541ee
Keithcutt <mayerhofnew@gmail.com>
2024-02-19 23:06:46 +0300

    Домашнее задание 9 спринта:
    Тема I. Стили и темы
    1. Повторябщиеся атрибуты у однотипных вью вынесены в стили.
    2. Для всех экранов добавлена поддержка темной темы.
    3. Убраны тени у кнопок.
    4. Подключены необходимые шрифты для текстов на экране.
    
    Тема II. Context и Intent
    1. Все строки с экранов перенесены в ресурсы.
    2. Реализованы кнопки «Поделиться приложением», «Написать разработчикам», «Пользовательское соглашение».
    
    Тема III. EditText и TextWatcher
    1. Реализован экран «Поиск» без выполнения поискового запроса и отображения списка результатов.
    
    Тема IV. Хранение данных. Начало
    1. Реализовано сохранение текста поискового запроса в жизненном цикле SearchActivity.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,77 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<MaterialButton>(R.id.back_btn)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.search_bar)
+        savedInstanceState ?: inputEditText.setText(savedInput)
+        val clearButton = findViewById<ImageView>(R.id.clear_icon)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+
+
+        val searchBarWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) {
+                    savedInput = s.toString()
+
+                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        inputEditText.addTextChangedListener(searchBarWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_INPUT, savedInput)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInput = savedInstanceState.getString(TEXT_INPUT, TEXT_DEF)
+    }
+
+    companion object {
+        const val TEXT_INPUT = "TEXT_INPUT"
+        const val TEXT_DEF = ""
+    }
+
+    private var savedInput: String? = null
 }
